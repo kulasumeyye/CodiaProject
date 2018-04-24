ae2137c57079eb2e4d1da2f8eeb918ac9dc07bd0
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2018-04-25 00:15:57 +0300

    Add files via upload

---
+++ Faktoriyel-V3/Faktoriyel3/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Faktoriyel3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int sonuc;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int deger = Convert.ToInt32(txtval.Text);
+            int faktoriyell=FaktoriyelHesapla(deger);
+            lblsonuc.Text = Convert.ToString(faktoriyell);
+
+
+           
+
+
+
+        }
+      public int FaktoriyelHesapla(int sayi)
+        {
+
+            if (sayi == 0)
+                return 1;
+            else
+              return  sayi * (FaktoriyelHesapla(sayi - 1));
+
+             
+           
+
+        }
+       
+    }
+}
