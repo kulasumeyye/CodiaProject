aa3177857b20df881715260dd6a27489dd9bbca4
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2018-04-25 00:14:30 +0300

    Add files via upload

---
+++ Faktoriyel-V2/Faktoriyel2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Faktoriyel2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.AutoSize = true;
+            this.lblsonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsonuc.Location = new System.Drawing.Point(204, 113);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(51, 20);
+            this.lblsonuc.TabIndex = 7;
+            this.lblsonuc.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtval
+            // 
+            this.txtval.Location = new System.Drawing.Point(33, 95);
+            this.txtval.Name = "txtval";
+            this.txtval.Size = new System.Drawing.Size(100, 20);
+            this.txtval.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(28, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FAKTORİYEL HESAPLA";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 275);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtval);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtval;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Faktoriyel-V2/Faktoriyel2/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Faktoriyel2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int deger = Convert.ToInt32(txtval.Text);
+            int faktoriyel = 1;
+            int i=deger;
+            while(i!=0)
+            {
+                faktoriyel = faktoriyel * i;
+                i = i - 1;
+            }
+
+            lblsonuc.Text = Convert.ToString(faktoriyel);
+        }
+    }
+}
