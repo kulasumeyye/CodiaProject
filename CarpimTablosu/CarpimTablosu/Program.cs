021488daf47cae2aad8e2f4531573542c7671771
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2018-04-29 01:12:07 +0300

    Add files via upload

---
+++ CarpimTablosu/CarpimTablosu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpimTablosu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t ****ÇARPIM TABLOSU****");
+            Console.WriteLine("---------------------------------------------");
+
+            for(int i=1;i<=10;i++)
+            {
+                for(int j=1;j<=10;j++)
+                {
+                    Console.WriteLine("\t  {0} x {1} = {2}", i, j, i * j);
+
+                }
+                Console.WriteLine("---------------------------------------------");
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
