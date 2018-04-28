c42bd150a0ad8da6b25a91f6918e9750ff7de535
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2018-04-29 01:27:23 +0300

    Add files via upload

---
+++ Faktoriyel4/Faktoriyel4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faktoriyel4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı Değerini Giriniz:");
+            int sayi=Convert.ToInt32(Console.ReadLine());
+
+            int faktoriyel = 1;
+            for(int i=1;i<=sayi;i++)
+            {
+                faktoriyel += faktoriyel * i;
+            }
+            Console.WriteLine("{0} sayısının Faktoriyeli= {1}", sayi, faktoriyel);
+            Console.ReadKey();
+        }
+    }
+}
