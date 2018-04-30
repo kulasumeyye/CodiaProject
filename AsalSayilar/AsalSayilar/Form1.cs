28615adb233e0ae267e3af9477984064f3621689
kulasumeyye <38705209+kulasumeyye@users.noreply.github.com>
2018-04-30 04:03:02 +0300

    Add files via upload

---
+++ AsalSayilar/AsalSayilar/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsalSayilar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYaz_Click(object sender, EventArgs e)
+        {
+             int sayi = Convert.ToInt32(txtSayi.Text);
+            int i,sayac=0;
+            for (i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                { sayac++;
+                break;
+                }
+                  
+
+              
+                    
+           }
+
+            if (sayac > 0)
+                label1.Text = ("sayı asal değil");
+
+            else
+                label1.Text = ("sayı asal ");
+
+
+
+            
+        }
+    }
+}
